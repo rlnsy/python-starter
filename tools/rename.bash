d6add85adc335467ee1d471883c5c553ff004063
Rowan Lindsay <rowan@rowanlindsay.com>
2021-10-03 22:39:43 -0700

    rename package script

---
+++ tools/rename.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+old=$1
+new=$2
+
+stat=0
+
+if [[ -z "$old" ]]; then
+		echo "need old name" && stat+=1
+fi
+
+if [[ -z "$new" ]]; then
+		echo "need new name" && stat+=1
+fi
+
+if [[ $stat != 0 ]]; then
+		echo "error"
+		exit $stat
+fi
+
+files=( "setup.py" "main.py" "__tests__.py" )
+
+for f in "${files[@]}"
+do
+		echo "$f"
+		sed -i "" -e "s/$old/$new/" "$f"
+done
+
+mv -v "$old" "$new"
